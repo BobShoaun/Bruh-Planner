ee486bf3d5b09b0460c49471938976bf9382bba7
sun-yuhan <yuhan25@hotmail.com>
2021-03-20 18:27:51 -0400

    add button works :)

--- bruh-planner/src/components/popover.vue
+++ bruh-planner/src/components/popover.vue
@@ -1,19 +1,64 @@
 <template>
-  <ion-content class="ion-padding">
-    <ion-list mode='md'>
-        <ion-item button>Add Course</ion-item>
-        <ion-item button>Add Assignment</ion-item>
-        <ion-item button>Add Test/Quiz</ion-item>
-    </ion-list>
-  </ion-content>
+    <ion-content class="ion-padding">
+        <ion-list mode="md">
+            <ion-item
+                button
+                v-on:click="
+                    addType = 'course';
+                    openModal();
+                "
+                >Add Course</ion-item
+            >
+            <ion-item
+                button
+                v-on:click="
+                    addType = 'assignment';
+                    openModal();
+                "
+                >Add Assignment</ion-item
+            >
+            <ion-item
+                button
+                v-on:click="
+                    addType = 'testquiz';
+                    openModal();
+                "
+                >Add Test/Quiz</ion-item
+            >
+        </ion-list>
+    </ion-content>
 </template>
-
 <script>
-import { IonContent } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import { IonContent, modalController } from "@ionic/vue";
+import Add from "./Add";
+import { addOutline } from "ionicons/icons";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'Popover',
-  components: { IonContent }
+    name: "Popover",
+    components: { IonContent },
+    data() {
+        return {
+            viewType: "month",
+            addType: "",
+        };
+    },
+    methods: {
+        async openModal() {
+            const modal = await modalController.create({
+                component: Add,
+                cssClass: "my-custom-class",
+                componentProps: {
+                    addType: this.addType,
+                },
+            });
+            return modal.present();
+        },
+    },
+    setup() {
+        return {
+            addOutline,
+        };
+    },
 });
 </script>
--- bruh-planner/src/views/Tab1.vue
+++ bruh-planner/src/views/Tab1.vue
@@ -1,83 +1,96 @@
 <template>
-  <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <!-- <ion-title>Calendar</ion-title> -->
-      </ion-toolbar>
-    </ion-header>
-	
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Calendar</ion-title>
-        </ion-toolbar>
-      </ion-header>
-		<vue-cal
-			xsmall
-			click-to-navigate
-			:time="false"
-			active-view="month"
-			:disable-views="['years', 'year', 'day']"
-			:min-date="minDate"
-			:max-date="maxDate"
-			style="height: 400px"
-			class="vertical-center">
-		</vue-cal>
-		<!-- fab placed to the bottom end -->
-		<ion-fab vertical="bottom" horizontal="end" slot="fixed">
-			<ion-fab-button @click="openPopover">
-				<ion-icon icon="add"></ion-icon>
-			</ion-fab-button>
-		</ion-fab>
-		<Dashboard />
-    </ion-content>
-  </ion-page>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <!-- <ion-title>Calendar</ion-title> -->
+            </ion-toolbar>
+        </ion-header>
+
+        <ion-content :fullscreen="true">
+            <ion-header collapse="condense">
+                <ion-toolbar>
+                    <ion-title size="large">Calendar</ion-title>
+                </ion-toolbar>
+            </ion-header>
+            <vue-cal
+                xsmall
+                click-to-navigate
+                :time="false"
+                active-view="month"
+                :disable-views="['years', 'year', 'day']"
+                :min-date="minDate"
+                :max-date="maxDate"
+                style="height: 400px"
+                class="vertical-center"
+            >
+            </vue-cal>
+            <ion-fab vertical="bottom" horizontal="end" slot="fixed">
+                <ion-fab-button @click="openPopover">
+                    <ion-icon :icon="addOutline" />
+                </ion-fab-button>
+            </ion-fab>
+        </ion-content>
+    </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon, popoverController } from '@ionic/vue';
-import 'vue-cal/dist/i18n/zh-cn.js'
-import 'vue-cal/dist/vuecal.css'
-import VueCal from 'vue-cal';
-import {defineComponent} from 'vue';
-import Popover from '../components/popover.vue'
-import Dashboard from '../components/Dashboard.vue'
+import {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonFab,
+    IonFabButton,
+    IonIcon,
+    popoverController,
+} from "@ionic/vue";
+import { addOutline } from "ionicons/icons";
+import "vue-cal/dist/i18n/zh-cn.js";
+import "vue-cal/dist/vuecal.css";
+import VueCal from "vue-cal";
+import { defineComponent } from "vue";
+import Popover from "../components/Popover.vue";
 
 export default defineComponent({
-	components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, VueCal, IonFab, IonFabButton, IonIcon, Dashboard },
-	methods: {
-		async openPopover(ev: Event) {
-			const popover = await popoverController.create({
-				component: Popover,
-				cssClass: '',
-				event: ev,
-				translucent: true
-			})
-			return popover.present();
-		}
-	}
-})
+    components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, VueCal, IonFab, IonFabButton, IonIcon },
+    methods: {
+        async openPopover(ev: Event) {
+            const popover = await popoverController.create({
+                component: Popover,
+                cssClass: "",
+                event: ev,
+                translucent: true,
+            });
+            return popover.present();
+        },
+    },
+    setup() {
+        return {
+            addOutline,
+        };
+    },
+});
 </script>
 
 <style scoped>
 .calendar {
-	display: grid;
-	grid-template-columns: repeat(7, 1fr);
-	grid-template-rows: 2em, repeat(5, 1fr);
-	row-gap: 0.2em;
-	column-gap: 0.2em;
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    grid-template-rows: 2em, repeat(5, 1fr);
+    row-gap: 0.2em;
+    column-gap: 0.2em;
 }
 
 .calendar-day {
-	height: 100px;
+    height: 100px;
 }
 
 .vertical-center {
-  margin: 0;
-  position: absolute;
-  top: 50%;
-  -ms-transform: translateY(-50%);
-  transform: translateY(-50%);
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    -ms-transform: translateY(-50%);
+    transform: translateY(-50%);
 }
-
 </style>
