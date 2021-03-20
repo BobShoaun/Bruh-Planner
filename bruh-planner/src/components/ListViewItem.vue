b984a617c21252bf59e32115a46af6584008c768
sun-yuhan <yuhan25@hotmail.com>
2021-03-20 16:54:05 -0400

    can drag list items :)

--- bruh-planner/src/components/ListViewItem.vue
+++ bruh-planner/src/components/ListViewItem.vue
@@ -1,42 +1,48 @@
 <template>
-	<div class="card">
-		<div class="card-body row justify-content-start">
-			<p class="col-1 d-flex justify-content-center align-items-center">
-				<i class="fas fa-lg fa-grip-vertical text-secondary"></i>
-			</p>
-			<div class="col">
-				<h2>{{ name }}</h2>
-				<h3>{{ course }}</h3>
-				<p>Estimated time: {{ estimatedTime }} hours</p>
-			</div>
-		</div>
-	</div>
+    <ion-item>
+		<ion-reorder slot="start"></ion-reorder>
+        <ion-label class="ion-text-wrap">
+            <ion-text color="primary">
+                <h1>{{ name }}</h1>
+            </ion-text>
+            <ion-text color="secondary">
+                <p>{{ course }}</p>
+            </ion-text>
+            <p>Estimated time: {{ estimatedTime }} hours</p>
+        </ion-label>
+    </ion-item>
 </template>
 
 <script>
-export default {
-	props: {
-		name: {
-			type: String,
-		},
-		course: {
-			type: String,
-		},
-		estimatedTime: {
-			type: Number,
-		},
-		startTime: {
-			type: String,
-		},
-		endTime: {
-			type: String,
-		},
-	},
-};
+import { IonReorder, IonItem } from "@ionic/vue";
+import { reorderThreeOutline } from "ionicons/icons";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    components: { IonReorder, IonItem },
+    props: {
+        name: {
+            type: String,
+        },
+        course: {
+            type: String,
+        },
+        estimatedTime: {
+            type: Number,
+        },
+        startTime: {
+            type: String,
+        },
+        endTime: {
+            type: String,
+        },
+    },
+    setup() {
+        return {
+            reorderThreeOutline,
+        };
+    },
+});
 </script>
 
-<style scoped>
-.card {
-	display: flex;
-}
-</style>
+<style scoped></style>
--- bruh-planner/src/views/Tab3.vue
+++ bruh-planner/src/views/Tab3.vue
@@ -1,56 +1,64 @@
 <template>
-  <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>List</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">List</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <section>
-		<ListItem
-			v-for="(event, index) in events"
-			:key="index"
-			:name="event.name"
-			:course="event.course"
-			:estimatedTime="event.estTime"
-		/>
-	</section>
-    </ion-content>
-  </ion-page>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>List</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content :fullscreen="true">
+            <ion-header collapse="condense">
+                <ion-toolbar>
+                    <ion-title size="large">List</ion-title>
+                </ion-toolbar>
+            </ion-header>
+            <section>
+                <ion-reorder-group @ionItemReorder="reorderPriority($event)" :disabled="false">
+                    <ListItem
+                        v-for="(event, index) in events"
+                        :key="index"
+                        :name="event.name"
+                        :course="event.course"
+                        :estimatedTime="event.estTime"
+                    />
+                </ion-reorder-group>
+            </section>
+        </ion-content>
+    </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ListItem from '../components/ListViewItem.vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonReorderGroup, IonContent } from "@ionic/vue";
+import ListItem from "../components/ListViewItem.vue";
 
-export default  {
-  name: 'List',
-  components: { ListItem, IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
-  data() {
-		return {
-			events: [
-				{
-					name: "Quiz 1",
-					course: "CSC222",
-					estTime: 2,
-				},
-				{
-					name: "Test 3",
-					course: "CSC900",
-					estTime: 8,
-				},
-				{
-					name: "Assignment 22",
-					course: "CSC318",
-					estTime: 10,
-				},
-			],
-		};
-	},
-}
+export default {
+    name: "List",
+    components: { ListItem, IonHeader, IonToolbar, IonTitle, IonContent, IonReorderGroup, IonPage },
+    data() {
+        return {
+            events: [
+                {
+                    name: "Quiz 1",
+                    course: "CSC222",
+                    estTime: 2,
+                },
+                {
+                    name: "Test 3",
+                    course: "CSC900",
+                    estTime: 8,
+                },
+                {
+                    name: "Assignment 22",
+                    course: "CSC318",
+                    estTime: 10,
+                },
+            ],
+        };
+    },
+    setup() {
+        const reorderPriority = (e: CustomEvent) => {
+            e.detail.complete();
+        };
+        return { reorderPriority };
+    },
+};
 </script>
